7a62a6e6700d71eaa43380f33d57d29c54933b2f
Daniel Schulz <faulolio@gmail.com>
2024-09-21 21:53:25 +0200

    build: compose

--- scripts/bootstrap.php
+++ scripts/bootstrap.php
@@ -1,7 +1,28 @@
 <?php
 declare(strict_types = 1);
 
+namespace Slothsoft;
+
+use Slothsoft\Core\ServerEnvironment;
+use Slothsoft\Farah\Dictionary;
+use Slothsoft\Farah\Kernel;
 use Slothsoft\Farah\Module\Module;
 
-Module::registerWithXmlManifestAndDefaultAssets('slothsoft@daniel-schulz.slothsoft.net', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets');
+@include __DIR__ . '/../../global/slothsoft.core.php';
+@include __DIR__ . '/../../global/slothsoft.core.xslt.php';
+@include __DIR__ . '/../../global/slothsoft.core.dbms.php';
+@include __DIR__ . '/../../global/slothsoft.farah.php';
+
+$root = dirname(__DIR__);
+
+ServerEnvironment::setRootDirectory($root);
+ServerEnvironment::setCacheDirectory($root . DIRECTORY_SEPARATOR . 'cache');
+ServerEnvironment::setLogDirectory($root . DIRECTORY_SEPARATOR . 'log');
+ServerEnvironment::setDataDirectory($root . DIRECTORY_SEPARATOR . 'data');
+
+Kernel::setCurrentSitemap('farah://slothsoft@daniel-schulz.slothsoft.net/sitemap');
+Kernel::setTrackingEnabled(false);
+Dictionary::setSupportedLanguages('en-us');
+
+Module::registerWithXmlManifestAndDefaultAssets('slothsoft@daniel-schulz.slothsoft.net', $root . DIRECTORY_SEPARATOR . 'assets');
 

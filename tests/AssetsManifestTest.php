1bcaf2d0d3434a2e84c3662705a5df50c2cc9c83
Daniel Schulz <faulolio@gmail.com>
2024-09-28 22:36:26 +0200

    build: update files

--- tests/AssetsManifestTest.php
+++ tests/AssetsManifestTest.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types = 1);
-namespace Slothsoft\Server\DanielSchulz;
+namespace Slothsoft\Server\DanielSchulz\Tests;
 
 use Slothsoft\Farah\ModuleTests\AbstractXmlManifestTest;
 

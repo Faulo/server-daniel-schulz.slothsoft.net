8dde6e04332a5abd68ee724bc431960ff163a15c
Daniel Schulz (Estelle) <faulolio@gmail.com>
2025-10-25 21:25:05 +0200

    fix: tests

--- tests/SitemapTest.php
+++ tests/SitemapTest.php
@@ -3,11 +3,11 @@ declare(strict_types = 1);
 namespace Slothsoft\Server\DanielSchulz\Tests;
 
 use Slothsoft\Farah\Configuration\AssetConfigurationField;
-use Slothsoft\Farah\ModuleTests\AbstractSitemapTest;
+use Slothsoft\FarahTesting\Module\AbstractSitemapTest;
 use Slothsoft\Farah\Module\Asset\AssetInterface;
 
 class SitemapTest extends AbstractSitemapTest {
-
+    
     protected static function loadSitesAsset(): AssetInterface {
         return (new AssetConfigurationField('farah://slothsoft@daniel-schulz.slothsoft.net/sitemap'))->getValue();
     }
